b599a05156bb9f8065b3c9f2685b754f46b57a14
songshuzhong <sshuzhong@outlook.com>
2020-09-01 22:45:45 +0800

    dist menu

---
+++ dist/precache-manifest.7ab3bacecaf79ec1be7d9df1e228b0b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c65c1e32f40043a2173c",
+    "url": "css/chunk-vendors.955cdc55.css"
+  },
+  {
+    "revision": "c65c1e32f40043a2173c",
+    "url": "js/chunks/chunk-vendors.795992.js"
+  },
+  {
+    "revision": "a8917b83ce883b6af05c",
+    "url": "css/index.17253312.css"
+  },
+  {
+    "revision": "a8917b83ce883b6af05c",
+    "url": "js/index.795992.js"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "1c224c772383a60b313f11e9df957bcb",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "a9df12fd623ec2170480949088d7a28e",
+    "url": "index.html"
+  }
+];
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.068694d559872f056c872e76b852db5a.js"
+  "precache-manifest.7ab3bacecaf79ec1be7d9df1e228b0b2.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "fe-fans-coupon"});
